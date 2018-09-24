8515a80fb2d9d08f88e24744e4b8d4e3681c8f9a
Roberto Sanchez <robertosancsanz@gmail.com>
2018-09-24 23:39:03 +0200

    include series repository

---
+++ app/src/main/java/com/android/es/roversanz/series/data/model/Serie.kt
@@ -0,0 +1,7 @@
+package com.android.es.roversanz.series.data.model
+
+data class Serie(
+        val id: Long,
+        val title: String,
+        val description: String
+)
--- app/src/main/java/com/android/es/roversanz/series/di/components/MainComponent.kt
+++ app/src/main/java/com/android/es/roversanz/series/di/components/MainComponent.kt
@@ -1,16 +1,18 @@
 package com.android.es.roversanz.series.di.components
 
 import android.content.Context
+import com.android.es.roversanz.series.data.repository.SerieRepository
 import com.android.es.roversanz.series.di.MySchedulers
 import com.android.es.roversanz.series.di.module.ApplicationModule
 import com.android.es.roversanz.series.di.module.LoggerModule
+import com.android.es.roversanz.series.di.module.RepositoryModule
 import com.android.es.roversanz.series.utils.ResourceProvider
 import com.android.es.roversanz.series.utils.logger.Logger
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class, LoggerModule::class])
+@Component(modules = [ApplicationModule::class, LoggerModule::class, RepositoryModule::class])
 interface MainComponent {
 
     fun provideContext(): Context
@@ -20,5 +22,7 @@ interface MainComponent {
     fun provideLogger(): Logger
 
     fun provideSchedulers(): MySchedulers
+
+    fun provideSerieRepository(): SerieRepository
 
 }
