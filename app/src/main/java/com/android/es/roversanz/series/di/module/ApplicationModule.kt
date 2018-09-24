b6085eff0adb7b1382aef88849332f15702aedc9
Roberto Sanchez <robertosancsanz@gmail.com>
2018-09-24 17:47:17 +0200

    Initialize Dagger 2

---
+++ app/src/main/java/com/android/es/roversanz/series/di/module/ApplicationModule.kt
@@ -0,0 +1,21 @@
+package com.android.es.roversanz.series.di.module
+
+import android.content.Context
+import com.android.es.roversanz.series.utils.ResourceProvider
+import com.android.es.roversanz.series.utils.ResourceProviderImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class ApplicationModule(private val ctx: Context) {
+
+    @Provides
+    @Singleton
+    internal fun provideContext() = ctx
+
+    @Provides
+    @Singleton
+    internal fun provideStringProvider(ctx: Context): ResourceProvider = ResourceProviderImpl(ctx)
+
+}
---
+++ app/src/main/java/com/android/es/roversanz/series/di/scopes/ActivityScope.kt
@@ -0,0 +1,6 @@
+package com.android.es.roversanz.series.di.scopes
+
+import javax.inject.Scope
+
+@Scope
+annotation class ActivityScope
