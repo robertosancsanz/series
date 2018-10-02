8627993b43d4bf67f8900a78b9208163708177cb
Roberto Sanchez <robertosancsanz@gmail.com>
2018-10-03 00:52:20 +0200

    include viewmodel on mainactivity

--- app/src/main/java/com/android/es/roversanz/series/presentation/ui/list/SeriesViewModel.kt
+++ app/src/main/java/com/android/es/roversanz/series/presentation/ui/list/SeriesViewModel.kt
@@ -1,4 +1,4 @@
-package com.android.es.roversanz.series.presentation.ui.main
+package com.android.es.roversanz.series.presentation.ui.list
 
 import android.arch.lifecycle.ViewModel
 import com.android.es.roversanz.series.usecases.series.GetSeriesListUseCase
