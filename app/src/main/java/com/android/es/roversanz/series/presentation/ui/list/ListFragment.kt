85b7ee8258360d20406ec53072091ba0fca683cf
Roberto Sanchez <robertosancsanz@gmail.com>
2018-10-04 17:48:53 +0200

    add some UI

--- app/src/main/java/com/android/es/roversanz/series/presentation/ui/list/ListFragment.kt
+++ app/src/main/java/com/android/es/roversanz/series/presentation/ui/list/ListFragment.kt
@@ -4,16 +4,21 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.es.roversanz.series.R
+import com.android.es.roversanz.series.domain.Serie
 import com.android.es.roversanz.series.presentation.MyApplication
 import com.android.es.roversanz.series.presentation.di.components.MainComponent
 import com.android.es.roversanz.series.presentation.di.scopes.FragmentScope
 import com.android.es.roversanz.series.utils.app
 import dagger.Component
+import kotlinx.android.synthetic.main.fragment_list_series.*
+import kotlinx.android.synthetic.main.item_serie.view.*
 import javax.inject.Inject
 
 class ListFragment : Fragment() {
@@ -29,20 +34,27 @@ class ListFragment : Fragment() {
         ViewModelProviders.of(this, factory)[SeriesListViewModel::class.java]
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
-            = inflater.inflate(R.layout.fragment_list_series, null)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_list_series, null)
+
+    private lateinit var seriesAdapter: SeriesAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         activity?.let { inject(it.app()) }
 
+        seriesAdapter = SeriesAdapter()
+        series_list.apply {
+            adapter = seriesAdapter
+            layoutManager = GridLayoutManager(context, 2)
+        }
+
         viewModel.getState().observe(this, Observer { state ->
             when (state) {
                 SeriesListState.INITIAL -> Log.d("LIST", "Initial State")
                 SeriesListState.BUSY -> Log.d("LIST", "Show Loading")
                 SeriesListState.EMPTY -> Log.d("LIST", "The list is empty")
-                is SeriesListState.DONE -> Log.d("LIST", "Show the list: ${state.data.size}")
+                is SeriesListState.DONE -> updateList(state.data)
                 is SeriesListState.ERROR -> Log.d("LIST", "Show the error")
                 else -> Log.d("LIST", "Something is wrong")
             }
@@ -52,6 +64,11 @@ class ListFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         viewModel.getState().removeObservers(this)
+    }
+
+    private fun updateList(list: List<Serie>) {
+        Log.d("LIST", "Show the list: ${list.size}")
+        seriesAdapter.updateSeries(list)
     }
 
     private fun inject(app: MyApplication) {
@@ -66,6 +83,36 @@ class ListFragment : Fragment() {
     internal interface ListFragmentComponent {
 
         fun inject(fragment: ListFragment)
+    }
+
+}
+
+class SeriesAdapter : RecyclerView.Adapter<SeriesAdapter.SeriesViewHolder>() {
+
+    private var series: List<Serie> = listOf()
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): SeriesViewHolder {
+        return SeriesViewHolder(LayoutInflater.from(p0.context).inflate(R.layout.item_serie, null))
+    }
+
+    override fun getItemCount() = series.size
+
+    override fun onBindViewHolder(holder: SeriesViewHolder, position: Int) {
+        holder.onBind(series[position])
+    }
+
+    fun updateSeries(list: List<Serie>) {
+        series = list
+        notifyDataSetChanged()
+    }
+
+    class SeriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun onBind(serie: Serie) {
+            itemView.serie_title.text = serie.title
+            itemView.serie_description.text = serie.description
+        }
+
     }
 
 }
