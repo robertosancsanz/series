86550b4a5597f4edd46d2e6826574b4052c02a23
Roberto Sanchez <robertosancsanz@gmail.com>
2018-10-04 17:49:26 +0200

    add an image url

--- app/src/main/java/com/android/es/roversanz/series/domain/LocalDataPersistence.kt
+++ app/src/main/java/com/android/es/roversanz/series/domain/LocalDataPersistence.kt
@@ -6,8 +6,8 @@ class LocalDataPersistence : DataPersistence {
 
     private val series: MutableList<Serie> by lazy {
         mutableListOf(
-                Serie(1, "El Cuento de la Criada", "Muy heavy"),
-                Serie(2, "Picky Blinders ", "Son mu malos"))
+                Serie(1, "El Cuento de la Criada", "Muy heavy", "https://www.ecestaticos.com/imagestatic/clipping/03b/c8f/03bc8fb84150aa179e63bc8898802516/imagen-sin-titulo.jpg?mtime=1531473691"),
+                Serie(2, "Picky Blinders ", "Son mu malos", "http://cadenaser00.epimg.net/ser/imagenes/2018/05/07/television/1525693994_243954_1525694097_noticia_normal.jpg"))
     }
 
     override fun fetchSeries(): List<Serie> = series
--- app/src/main/java/com/android/es/roversanz/series/domain/Serie.kt
+++ app/src/main/java/com/android/es/roversanz/series/domain/Serie.kt
@@ -3,5 +3,6 @@ package com.android.es.roversanz.series.domain
 data class Serie(
         val id: Long,
         val title: String,
-        val description: String
+        val description: String,
+        val url:String
 )
