68d9164976235e7c780e61101de3c2812d8df6da
Roberto Sanchez <robertosancsanz@gmail.com>
2018-10-16 22:01:56 +0200

    Include livedata on DownloadManager

--- app/src/main/java/com/android/es/roversanz/series/usecases/download/DownloadFileUseCase.kt
+++ app/src/main/java/com/android/es/roversanz/series/usecases/download/DownloadFileUseCase.kt
@@ -1,41 +1,66 @@
 package com.android.es.roversanz.series.usecases.download
 
+import android.arch.lifecycle.Observer
+import android.util.Log
 import com.android.es.roversanz.series.data.DownloadManager
-import com.android.es.roversanz.series.data.DownloadManager.DownloadFileUseCaseListener
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState.COMPLETED
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState.ERROR
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState.PROGRESS
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState.QUEUED
 import com.android.es.roversanz.series.domain.Serie
 import com.android.es.roversanz.series.usecases.UseCase
 import com.android.es.roversanz.series.usecases.series.SerieDownloaded
 
 class DownloadFileUseCase(private val downloadManager: DownloadManager) : UseCase {
 
+    companion object {
+        const val TAG = "Download"
+    }
+
+    private var observer: Observer<DownloadManagerState>? = null
+
     operator fun invoke(
-            serieToDownload: Serie,
+            serie: Serie,
             onSuccess: (SerieDownloaded) -> Unit,
             onError: (SerieDownloaded) -> Unit,
             onQueued: ((SerieDownloaded) -> Unit)? = null,
             onProgress: ((SerieDownloaded) -> Unit)? = null) {
 
-        val listener = object : DownloadFileUseCaseListener {
-            override fun onQueued(serieDownloaded: SerieDownloaded) {
-                onQueued?.invoke(serieDownloaded)
-            }
+        Log.d(TAG, "Downloading ${serie.title} on $this")
 
-            override fun onProgress(serieDownloaded: SerieDownloaded) {
-                onProgress?.invoke(serieDownloaded)
-            }
+        //FIXME: The observable is the same, but in this way, it´s overrinding the callbacks, so losing the first one
+        if (observer == null) {
+            observer = Observer<DownloadManagerState> { state ->
+                when (state) {
+                    is QUEUED    -> {
+                        Log.d(TAG, "Queued ${serie.title}")
+                        onQueued?.invoke(state.serieDownloaded)
+                    }
+                    is PROGRESS  -> {
+                        Log.d(TAG, "PROGRESS ${serie.title}")
+                        onProgress?.invoke(state.serieDownloaded)
+                    }
+                    is COMPLETED -> {
+                        Log.d(TAG, "COMPLETED ${serie.title}")
+                        onSuccess.invoke(state.serieDownloaded)
+                        removeObserver()
+                    }
+                    is ERROR     -> {
+                        Log.d(TAG, "ERROR ${serie.title}")
+                        onError.invoke(state.serieDownloaded)
+                        removeObserver()
+                    }
+                }
+            }.apply { downloadManager.state.observeForever(this) }
 
-            override fun onSuccess(serieDownloaded: SerieDownloaded) {
-                onSuccess.invoke(serieDownloaded)
-                downloadManager.removeCallbacks(this)
-            }
+        }
+        downloadManager.download(serie)
+    }
 
-            override fun onError(serieDownloaded: SerieDownloaded) {
-                onError.invoke(serieDownloaded)
-                downloadManager.removeCallbacks(this)
-            }
-        }.apply { downloadManager.addToCallbacks(this) }
-
-        downloadManager.download(serieToDownload)
+    private fun removeObserver() {
+        observer?.let { downloadManager.state.removeObserver(it) }
+        observer = null
     }
 
 }
--- app/src/main/java/com/android/es/roversanz/series/usecases/download/ResumeDownloadFileUseCase.kt
+++ app/src/main/java/com/android/es/roversanz/series/usecases/download/ResumeDownloadFileUseCase.kt
@@ -1,26 +1,48 @@
 package com.android.es.roversanz.series.usecases.download
 
+import android.arch.lifecycle.Observer
+import android.util.Log
 import com.android.es.roversanz.series.data.DownloadManager
-import com.android.es.roversanz.series.data.DownloadManager.DownloadFileUseCaseListener
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState
+import com.android.es.roversanz.series.data.DownloadManager.DownloadManagerState.RESUMED
 import com.android.es.roversanz.series.domain.Serie
 import com.android.es.roversanz.series.usecases.UseCase
 import com.android.es.roversanz.series.usecases.series.SerieDownloaded
 
 class ResumeDownloadFileUseCase(private val downloadManager: DownloadManager) : UseCase {
 
+    companion object {
+        const val TAG = "Download"
+    }
+
+    private var observer: Observer<DownloadManagerState>? = null
+
     operator fun invoke(
             serie: Serie,
             callback: ((SerieDownloaded) -> Unit)? = null) {
 
-        callback?.let { cb ->
-            val listener = object : DownloadFileUseCaseListener {
-                override fun onResumed(serieDownloaded: SerieDownloaded) {
-                    cb.invoke(serieDownloaded)
-                    downloadManager.removeCallbacks(this)
-                }
-            }.apply { downloadManager.addToCallbacks(this) }
+
+        Log.d(TAG, "Resuming ${serie.title} on $this")
+
+        callback?.let {
+            if (observer == null) {
+                observer = Observer<DownloadManagerState> { state ->
+                    when (state) {
+                        is RESUMED -> {
+                            it.invoke(state.serieDownloaded)
+                            removeObserver()
+                        }
+                    }
+                }.apply { downloadManager.state.observeForever(this) }
+            }
         }
+
         downloadManager.resume(serie.id)
+    }
+
+    private fun removeObserver() {
+        observer?.let { downloadManager.state.removeObserver(it) }
+        observer = null
     }
 
 
